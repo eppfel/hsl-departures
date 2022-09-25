e23df9e40050a593862e838fd65f505c8c19b1f5
Felix A. Epp <mail@felix.science>
2022-09-25 18:58:52 +0300

    Add stop, store in struct, cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
-#include <Ticker.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
@@ -14,14 +13,22 @@ WiFiMulti wifiMulti;
 
 const static int updateCycle = 5;
 int lastUpdate = 0;
-Ticker tickerDepartureT;
+
+#define NUM_STOPTIMES 6
+struct stoptime_t
+{
+  int stopId;
+  int realtimeDeparture;
+  const char* headsign;
+};
+stoptime_t stoptimes[NUM_STOPTIMES];
 
 void updateDepartureTimes() {
   // check departure times frok HSL API and update list of upcoming departures
   if((wifiMulti.run() == WL_CONNECTED)) {
 
     HTTPClient http;
-    StaticJsonDocument<400> doc;
+    StaticJsonDocument<800> doc;
 
     //client.setCACert(root_ca);
     Serial.print("[HTTP] begin...\n");
@@ -32,7 +39,7 @@ void updateDepartureTimes() {
     //http.addHeader("Authorization",  "Bearer actual-token-goes-here");
 
     // send POST request
-    doc["query"] = "{\n stop: stop(id: \"HSL:2232238\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeArrival\n headsign\n }\n }\n}\n";
+    doc["query"] = "{\n stop1: stop(id: \"HSL:2232238\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeDeparture\n realtime\n headsign\n }\n }\n stop2: stop(id: \"HSL:2232239\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeDeparture\n realtime\n headsign\n }\n }\n stop3: stop(id: \"HSL:2232211\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeDeparture\n realtime\n headsign\n }\n }\n}\n";
     doc["variables"] = nullptr;
     String payload; //= "{\"query\": \"{stops {name}}\" }";
     serializeJson(doc, payload);
@@ -43,10 +50,31 @@ void updateDepartureTimes() {
       deserializeJson(doc, http.getString());
       serializeJson(doc["data"], Serial);
       Serial.println();
-      tft.println("OK!");
+
+      tft.setCursor(0,0);
+      tft.fillScreen(TFT_BLUE);
+
+      //Transpose incoming data into a list of departures
+
+      stoptime_t tmpStoptimes[NUM_STOPTIMES];
+      for (size_t i = 0; i < NUM_STOPTIMES; i++)
+      {
+        int stop = (int) i / 2 + 1;
+        char stopN[6];
+        snprintf(stopN, 6, "stop%d", stop);
+        tmpStoptimes[i].stopId = stop;
+        tmpStoptimes[i].realtimeDeparture = doc["data"][stopN]["stoptimesWithoutPatterns"][i%2]["realtimeDeparture"];
+        tmpStoptimes[i].headsign = doc["data"][stopN]["stoptimesWithoutPatterns"][i%2]["headsign"];
+        Serial.println(stopN);
+        Serial.println(tmpStoptimes[i].realtimeDeparture);
+        Serial.println(tmpStoptimes[i].headsign);
+        tft.printf("%d: %s\n",tmpStoptimes[i].realtimeDeparture, tmpStoptimes[i].headsign);
+      }
+
+      
+
     } else {
-      //Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(response).c_str());
-      Serial.print("[HTTO] Error: ");
+      Serial.print("[HTTP] Error: ");
       Serial.println(response);
       tft.println("Error!");
       payload = http.getString();
@@ -64,12 +92,6 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
-  // for(uint8_t t = 4; t > 0; t--) {
-  //     Serial.printf("[SETUP] WAIT %d...\n", t);
-  //     Serial.flush();
-  //     delay(1000);
-  // }
-
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLUE);
@@ -80,8 +102,6 @@ void setup() {
   tft.setTextSize(2);
   delay(10);
   wifiMulti.addAP(SSID, WIFI_PASSWORD);
-  
-  //tickerDepartureT.attach(5, updateDepartureTimes);
 }
 
 void loop() {
