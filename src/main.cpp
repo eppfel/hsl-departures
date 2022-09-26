018c0874b56cfc65b20a61e867351c8e685d0823
Felix A. Epp <mail@felix.science>
2022-09-26 08:18:51 +0300

    Sort departures and print to screen

--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,21 @@
 #include <credentials.h>
 #include "time.h"
 #include "sntp.h"
+#include <algorithm>
 
 const char* ntpServer1 = "pool.ntp.org";
 const char* ntpServer2 = "time.nist.gov";
 
 const char* time_zone = "EET-2EEST,M3.5.0/3,M10.5.0/4";  // TimeZone rule for Europe/Helsinki including daylight adjustment rules (optional)
 
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("No time available (yet)");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
-// Callback function (get's called when time adjusts via NTP)
-void timeavailable(struct timeval *t)
-{
-  Serial.println("Got time adjustment from NTP!");
-  printLocalTime();
-}
-
-
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+int lastRefresh;
+const static int refreshCycle = 500;
 
 WiFiMulti wifiMulti;
 
 const static int updateCycle = 5;
 int lastUpdate = 0;
-
 #define NUM_STOPTIMES 6
 struct stoptime_t
 {
@@ -47,6 +31,26 @@ struct stoptime_t
   const char* headsign;
 };
 stoptime_t stoptimes[NUM_STOPTIMES];
+
+bool cmpfunc(stoptime_t a, stoptime_t b) { 
+  return a.realtimeDeparture < b.realtimeDeparture; 
+}
+
+int getLocalTimeOfTheDay()
+{
+  struct tm ti;
+  if(!getLocalTime(&ti,1000)){
+    Serial.println("Could not obtain time info");
+    return -1;
+  }
+  return (ti.tm_hour * 3600 + ti.tm_min * 60 + ti.tm_sec);
+}
+
+// Callback function (get's called when time adjusts via NTP)
+void timeavailable(struct timeval *t)
+{
+  Serial.printf("Got time adjustment from NTP!");
+}
 
 void updateDepartureTimes() {
   // check departure times frok HSL API and update list of upcoming departures
@@ -61,7 +65,6 @@ void updateDepartureTimes() {
 
     // add necessary headers
     http.addHeader("Content-Type",   "application/json");
-    //http.addHeader("Authorization",  "Bearer actual-token-goes-here");
 
     // send POST request
     doc["query"] = "{\n stop1: stop(id: \"HSL:2232238\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeDeparture\n realtime\n headsign\n }\n }\n stop2: stop(id: \"HSL:2232239\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeDeparture\n realtime\n headsign\n }\n }\n stop3: stop(id: \"HSL:2232211\") {\n name\n stoptimesWithoutPatterns(numberOfDepartures: 2) {\n realtimeDeparture\n realtime\n headsign\n }\n }\n}\n";
@@ -76,27 +79,20 @@ void updateDepartureTimes() {
       serializeJson(doc["data"], Serial);
       Serial.println();
 
-      tft.setCursor(0,0);
-      tft.fillScreen(TFT_BLUE);
-
       //Transpose incoming data into a list of departures
-
-      stoptime_t tmpStoptimes[NUM_STOPTIMES];
       for (size_t i = 0; i < NUM_STOPTIMES; i++)
       {
         int stop = (int) i / 2 + 1;
         char stopN[6];
         snprintf(stopN, 6, "stop%d", stop);
-        tmpStoptimes[i].stopId = stop;
-        tmpStoptimes[i].realtimeDeparture = doc["data"][stopN]["stoptimesWithoutPatterns"][i%2]["realtimeDeparture"];
-        tmpStoptimes[i].headsign = doc["data"][stopN]["stoptimesWithoutPatterns"][i%2]["headsign"];
+        stoptimes[i].stopId = stop;
+        stoptimes[i].realtimeDeparture = doc["data"][stopN]["stoptimesWithoutPatterns"][i%2]["realtimeDeparture"];
+        stoptimes[i].headsign = doc["data"][stopN]["stoptimesWithoutPatterns"][i%2]["headsign"];
         Serial.println(stopN);
-        Serial.println(tmpStoptimes[i].realtimeDeparture);
-        Serial.println(tmpStoptimes[i].headsign);
-        tft.printf("%d: %s\n",tmpStoptimes[i].realtimeDeparture, tmpStoptimes[i].headsign);
+        Serial.println(stoptimes[i].realtimeDeparture);
+        Serial.println(stoptimes[i].headsign);
       }
-
-      
+      std::sort(std::begin(stoptimes), std::end(stoptimes), cmpfunc);
 
     } else {
       Serial.print("[HTTP] Error: ");
@@ -116,6 +112,7 @@ void updateDepartureTimes() {
 void setup() {
   Serial.begin(115200);
 
+  //setup NTP time synchronisation
   sntp_set_time_sync_notification_cb( timeavailable );
   sntp_servermode_dhcp(1);
   configTzTime(time_zone, ntpServer1, ntpServer2);
@@ -135,7 +132,24 @@ void setup() {
 void loop() {
   if (millis() > (lastUpdate + updateCycle*1000)) {
     lastUpdate = millis();
-    printLocalTime();
     updateDepartureTimes();
+  }
+  if (millis() > (lastRefresh + refreshCycle)) {
+    lastRefresh = millis();
+    if (stoptimes[0].realtimeDeparture > 0) {
+      tft.setCursor(0,0);
+      tft.fillScreen(TFT_BLUE);
+      struct tm timeinfo;
+      if(!getLocalTime(&timeinfo)){
+        Serial.println("No time available (yet)");
+      }
+      else {
+        tft.println(&timeinfo, "%H:%M:%S");
+      }
+      for (size_t i = 0; i < 4; i++)
+      {
+        tft.printf("%d: %d\n", stoptimes[i].realtimeDeparture - getLocalTimeOfTheDay(), stoptimes[i].stopId);
+      }
+    }
   }
 }
